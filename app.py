6036343ad126b550ef3962f68358f6c484798b10
Chiragvadgamaa <115563333+Chiragvadgamaa@users.noreply.github.com>
2026-09-09 16:53:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,17 @@ def hello_world():
     return 'Hello from spidy'
 
 
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from spidy'
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
   
