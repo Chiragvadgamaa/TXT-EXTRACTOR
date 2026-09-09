e955dae85ec0a41d50f0ded8457c8e86bd106e2d
Chiragvadgamaa <115563333+Chiragvadgamaa@users.noreply.github.com>
2026-09-09 17:44:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 import os
-import subprocess
+import asyncio
+import threading
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Bot is active!'
+    return 'Bot is Running Successfully!'
 
-if __name__ == "__main__":
-    # Bot module/script ko start karne ke liye
-    subprocess.Popen(["python3", "-m", "Extractor"])
-    
+def run_flask():
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    # Flask app ko alag thread me chalayein taaki port active rahe
+    threading.Thread(target=run_flask, daemon=True).start()
     
-  
+    # Bot script ko run karein
+    os.system("python3 -m Extractor")
+    
