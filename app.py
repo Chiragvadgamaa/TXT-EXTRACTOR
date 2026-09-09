bdc1ae5eccf318f640f2345f48567c28ad5262a0
Chiragvadgamaa <115563333+Chiragvadgamaa@users.noreply.github.com>
2026-09-09 17:08:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import os
+import subprocess
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from spidy'
+    return 'Bot is active!'
 
 if __name__ == "__main__":
+    # Bot module/script ko start karne ke liye
+    subprocess.Popen(["python3", "-m", "Extractor"])
+    
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
     
